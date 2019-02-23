03959394b1fff2ceae7f6b75a6c5fd44d3571fa1
khengschew <khengc@gmail.com>
2019-02-23 15:13:49 -0800

    Update proxy to deploy to EC2 using docker

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('default'));
+app.use(morgan('dev'));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
@@ -14,3 +15,8 @@ app.listen(port, () => {
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
 app.use('/:id', express.static(path.join(__dirname, '..', 'client')));
+
+app.use('/restaurants/:id', proxy({ target: 'http://ec2-3-92-226-206.compute-1.amazonaws.com' }));
+app.use('/articles/:id', proxy({ target: 'http://ec2-3-92-226-206.compute-1.amazonaws.com' }));
+app.use('/reviews/:id', proxy({ target: 'http://ec2-3-92-226-206.compute-1.amazonaws.com' }));
+app.use('/api/:id', proxy({ target: 'http://ec2-54-67-103-109.us-west-1.compute.amazonaws.com' }));
